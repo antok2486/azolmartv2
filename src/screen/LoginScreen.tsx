0f5c8d17134b23388d832ae9a053a806fc9fbc38
Bambang Suprianto <antok2486@gmail.com>
2023-06-30 06:47:35 +0700

    new file:   .vscode/settings.json 	modified:   src/screen/LoginScreen.tsx

--- src/screen/LoginScreen.tsx
+++ src/screen/LoginScreen.tsx
@@ -1,9 +1,29 @@
-import { View, Text, Button, Image, TouchableOpacity, TextInput } from 'react-native'
-import React from 'react'
+import { View, Text, Button, Image, TouchableOpacity, TextInput, BackHandler, Alert } from 'react-native'
+import React, { useEffect } from 'react'
 import { style } from '../utils/style'
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 
 export default function LoginScreen({ ...props }) {
+    useEffect(() => {
+        const backAction = () => {
+            Alert.alert('Hold on!', 'Are you sure you want to go back?', [
+                {
+                    text: 'Cancel',
+                    onPress: () => null,
+                    style: 'cancel',
+                },
+                { text: 'YES', onPress: () => BackHandler.exitApp() },
+            ]);
+            return true;
+        };
+
+        const backHandler = BackHandler.addEventListener(
+            'hardwareBackPress',
+            backAction,
+        );
+
+        return () => backHandler.remove();
+    }, [])
 
     return (
         <View style={{ alignItems: 'center' }}>
@@ -18,14 +38,14 @@ export default function LoginScreen({ ...props }) {
                     <View style={{ position: 'absolute', left: 10 }} >
                         <FontAwesome5 name='user' size={18} />
                     </View>
-                    <TextInput placeholder='Email / Phone Number' style={style.loginInput} onChangeText={(text) => null } />
+                    <TextInput placeholder='Email / Phone Number' style={style.loginInput} onChangeText={(text) => null} />
                 </View>
 
                 <View style={{ width: '100%', marginTop: 20, flexDirection: 'row', alignItems: 'center' }}>
                     <View style={{ position: 'absolute', left: 10 }} >
                         <FontAwesome5 name='lock' size={18} />
                     </View>
-                    <TextInput placeholder='Password' secureTextEntry={true} style={style.loginInput} onChangeText={(text) => null } />
+                    <TextInput placeholder='Password' secureTextEntry={true} style={style.loginInput} onChangeText={(text) => null} />
                 </View>
 
                 <TouchableOpacity style={style.loginButton} onPress={() => null} >
